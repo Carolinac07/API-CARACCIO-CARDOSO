2006dc0b216e3dcd4d808f97a05d7e7ffdb01784
Carolina <166563593+Carolinac07@users.noreply.github.com>
2025-11-03 14:41:35 -0300

    Add files via upload

--- managers/conexionManagerSupaBase.py
+++ managers/conexionManagerSupaBase.py
@@ -1,13 +1,13 @@
 import os
 from typing import Generator
-
 import psycopg
 from dotenv import load_dotenv
 
 load_dotenv()
 passwordDB=os.getenv("SUPABASEPASSWORD")
 
-url=f"postgresql://postgres.wuvgiamzxxlyazxpdqzy:{passwordDB}@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
+url=f"postgresql://postgres.djrwsjcdbrssflbndlum:{passwordDB}@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
+
 
 def getCursor()-> Generator [psycopg.Cursor, None, None]:
     conn=psycopg.connect(url, sslmode="require")
--- managers/pedidosManager.py
+++ managers/pedidosManager.py
@@ -3,11 +3,11 @@ from models import Pedido
 
 class PedidosManager:
     def obtenerPedidos(self, cursor: psycopg.Cursor):
-        res = cursor.execute("SELECT pedidos.pedido_id,clientes.nombre AS cliente,productos.nombre AS producto,productos.precio,pedidos.cantidad,(productos.precio * pedidos.cantidad) AS totalFROM pedidosINNER JOIN clientes ON pedidos.cliente_id = clientes.id_clienteINNER JOIN productos ON pedidos.producto_id = productos.producto_id").fetchall()
+        res = cursor.execute("SELECT pedidos.pedido_id,clientes.nombre AS cliente,productos.nombre AS producto,productos.precio,pedidos.cantidad,(productos.precio * pedidos.cantidad) AS total FROM pedidos INNER JOIN clientes ON pedidos.cliente_id = clientes.id_clientes INNER JOIN productos ON pedidos.producto_id = productos.producto_id").fetchall()
         return [
             {"producto": row[0], "precio": row[1], "cliente": row[2]} for row in res
         ]
 
-    def insertarPedido(self, pedido: Pedido, cursor: psycopg.Cursor):
-        cursor.execute ("INSERT INTO pedidos(cliente_id, producto_id, cantidad)VALUES (%s,%s,%s)",(pedido.cliente_id, pedido.producto_id, pedido.cantidad))
+    def insertarPedido(self, pedido: pedido, cursor: psycopg.Cursor):
+        cursor.execute ("INSERT INTO pedidos(clientes_id, producto_id, cantidad)VALUES (%s,%s,%s)",(pedido.cliente_id, pedido.producto_id, pedido.cantidad))
         return "Pedido agregado."
